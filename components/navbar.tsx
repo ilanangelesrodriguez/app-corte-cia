f488ca4fd5a3c1b24390a0eed44d2e447f4dcd49
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-21 20:18:41 -0500

    chore: remove unused Button import from navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,6 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar"
-import { Button } from "@heroui/button"
 import { Link } from "@heroui/link"
 import NextLink from "next/link"
 import clsx from "clsx"
