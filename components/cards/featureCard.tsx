f1d3bd2aead7b42fd6348970bf27f0da2422ba69
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-25 23:53:49 -0500

    chore: modificar componentes de caracteristicas

--- components/cards/featureCard.tsx
+++ components/cards/featureCard.tsx
@@ -10,13 +10,13 @@ export const FeatureCard: React.FC<FeatureCardProps> = ({ icon, title, descripti
         whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
     >
         <motion.div
-            className="flex items-center mb-4"
+            className="flex items-center mb-4 justify-center gap-2"
             initial={{ x: -20, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ delay: 0.2, duration: 0.5 }}
         >
             {icon}
-            <h3 className="text-xl font-semibold m-auto text-gray-800 dark:text-white">{title}</h3>
+            <h3 className="text-lg md:text-xl text-left font-semibold text-gray-800 dark:text-white">{title}</h3>
         </motion.div>
         <motion.p
             className="text-gray-600 dark:text-gray-300"
